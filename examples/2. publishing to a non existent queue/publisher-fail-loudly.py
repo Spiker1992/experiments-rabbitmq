c2bfb25b40dcf323147eb71cae14b0314b30af21
Vlad <68771766+Spiker1992@users.noreply.github.com>
2025-08-13 20:49:01 +0000

    Add publisher examples for RabbitMQ with different failure modes for when queue doesn't exist

---
+++ examples/2. publishing to a non existent queue/publisher-fail-loudly.py
@@ -0,0 +1,37 @@
+# Reference: https://www.rabbitmq.com/docs/publishers#amqp-0-9-1
+# Publisher sends a message to a RabbitMQ queue
+#
+# run `docker exec rabbitmq rabbitmqctl list_queues` to see messages in the queue
+# 
+# After deleting the queue, by sending `delete_queue` message, new messages that you send
+# will fail loudly due to confirm delivery being enabled and mandatory set to True.
+import pika
+
+
+# Connect to RabbitMQ server
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Declare a queue
+channel.queue_declare(queue='hello')
+channel.confirm_delivery() # Enable publisher confirms - this will raise an exception if the message cannot be delivered
+
+# Infinite loop to prompt user for messages and publish to queue
+try:
+    while True:
+        message = input("Enter a message to publish (Ctrl+C to exit): ")
+        if message == "delete_queue":
+            channel.queue_delete(queue='hello')
+            print("Queue 'hello' deleted. You can now publish messages to a non-existent queue.")
+        else:
+            channel.basic_publish(
+                exchange='', 
+                routing_key='hello', 
+                body=message, 
+                mandatory=True # mandatory=True ensures that the message is returned if the queue does not exist
+            )
+            print(f"[x] Published '{message}'")
+except KeyboardInterrupt:
+    print("\nExiting publisher...")
+finally:
+    connection.close()
---
+++ examples/2. publishing to a non existent queue/publisher-silent-loss.py
@@ -0,0 +1,32 @@
+# Reference: https://www.rabbitmq.com/docs/publishers#amqp-0-9-1
+# Publisher sends a message to a RabbitMQ queue
+# run `docker exec rabbitmq rabbitmqctl list_queues` to see messages in the queue
+# 
+# After connection is opened we can delete the queue
+# with `docker exec rabbitmq rabbitmqctl delete_queue hello`
+#
+# After deleting the queue, messages are silently dropped
+import pika
+
+# Connect to RabbitMQ server
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Declare a queue
+channel.queue_declare(queue='hello')
+
+
+# Infinite loop to prompt user for messages and publish to queue
+try:
+    while True:
+        message = input("Enter a message to publish (Ctrl+C to exit): ")
+        if message == "delete_queue":
+            channel.queue_delete(queue='hello')
+            print("Queue 'hello' deleted. You can now publish messages to a non-existent queue.")
+        else:
+            channel.basic_publish(exchange='', routing_key='hello', body=message)
+            print(f"[x] Sent '{message}'")
+except KeyboardInterrupt:
+    print("\nExiting publisher...")
+finally:
+    connection.close()
