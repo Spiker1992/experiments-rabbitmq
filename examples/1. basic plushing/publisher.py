dfab424296062322192d6c9f0e00d146b40a7058
Vlad <68771766+Spiker1992@users.noreply.github.com>
2025-08-13 20:47:52 +0000

    Added basic example of sending and receiving data

---
+++ examples/1. basic plushing/publisher.py
@@ -0,0 +1,18 @@
+# Reference: https://www.rabbitmq.com/tutorials/tutorial-one-python
+# Publisher sends a message to a RabbitMQ queue
+# run `docker exec rabbitmq rabbitmqctl list_queues` to see messages in the queue
+import pika
+
+# Connect to RabbitMQ server
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Declare a queue
+channel.queue_declare(queue='hello')
+
+# Send a message
+channel.basic_publish(exchange='', routing_key='hello', body='Hello RabbitMQ!')
+print("[x] Sent 'Hello RabbitMQ!'")
+
+# Close the connection
+connection.close()
