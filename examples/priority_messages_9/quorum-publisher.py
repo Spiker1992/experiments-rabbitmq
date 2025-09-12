4fbdad60dbce9e7b69f627bf46e8711b7795e5b5
Vlad Ogir <v.v.ogir@outlook.com>
2025-09-12 20:54:00 +0000

    docs: added expected output for quorum queue when using priority messages

--- examples/priority_messages_9/quorum-publisher.py
+++ examples/priority_messages_9/quorum-publisher.py
@@ -12,10 +12,20 @@ for i in range(5):
     channel.basic_publish(exchange='', routing_key=QUEUE_NAME_QUORUM, body='Hello RabbitMQ!', priority=LOW_PRIORITY)
     print("[x] Sent a low prority message")
 
-channel.basic_publish(exchange='', routing_key=QUEUE_NAME_QUORUM, body='Hello RabbitMQ!')
-print("[x] Sent a high priority message", priority=HIGH_PRIORITY)
+for i in range(3):
+    channel.basic_publish(exchange='', routing_key=QUEUE_NAME_QUORUM, body='Hello RabbitMQ!')
+    print("[x] Sent a high priority message", priority=HIGH_PRIORITY)
 
-# expected out
+# expected output 
+# [x] Sent a low prority message (first one)
+# [x] Sent a high prority message (next two high priority are pushed to consumer)
+# [x] Sent a high prority message (this follows the 2 high for 1 low messages)
+# [x] Sent a low prority message (this is the next low priority message due to 2:1 ratio)
+# [x] Sent a high prority message (last high priority message)
+# [x] Sent a low prority message (the rest of the low priority messages)
+# [x] Sent a low prority message
+# [x] Sent a low prority message
+
 
 # Close the connection
 connection.close()
