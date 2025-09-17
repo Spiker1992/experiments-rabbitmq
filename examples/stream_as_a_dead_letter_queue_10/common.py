be4ef476540411a613721947569a13096df7531e
Vlad Ogir <v.v.ogir@outlook.com>
2025-09-17 19:56:30 +0000

    feat: added queues for streaming - to show case how stream can be used as a dlx

---
+++ examples/stream_as_a_dead_letter_queue_10/common.py
@@ -0,0 +1,28 @@
+import pika
+from config import RABBITMQ_HOST
+
+# It is recommended that all quorum queues have a dead letter configuration of some sort to
+#  ensure messages aren't dropped and lost unintentionally. Using a single stream for a low
+#  priority dead letter policy is a good, low resource way to ensure dropped messages are
+#  retained for some time after.
+# https://www.rabbitmq.com/docs/quorum-queues#poison-message-handling
+
+QUEUE_NAME_QUORUM = "stream_as_a_dead_letter_queue_10_quorum"
+STREAM_NAME = "stream_as_a_dead_letter_queue_10_stream" 
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+channel = connection.channel()
+
+channel.queue_declare(queue=STREAM_NAME, durable=True, exclusive=False, auto_delete=False, arguments={"x-queue-type": "stream"})
+channel.exchange_declare(exchange='dead_letter_exchange')
+channel.queue_bind(queue=STREAM_NAME, exchange='dead_letter_exchange', routing_key=STREAM_NAME)
+
+# Create a queue
+channel.queue_declare(queue=QUEUE_NAME_QUORUM, 
+durable=True, 
+arguments={
+    "x-queue-type": "quorum",
+    "x-dead-letter-exchange": "dead_letter_exchange",
+    "x-dead-letter-routing-key": STREAM_NAME
+})
